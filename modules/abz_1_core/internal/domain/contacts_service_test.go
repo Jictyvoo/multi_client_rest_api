52a9df817b7fbb99eb131ecdd498c0692d49ac75
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-06-06 23:37:02 -0300

    :cactus: test: Add `ContactsService` test

---
+++ modules/abz_1_core/internal/domain/contacts_service_test.go
@@ -0,0 +1,90 @@
+package domain
+
+import (
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/jictyvoo/multi_client_rest_api/modules/abz_1_core/internal/domain/entities"
+	"github.com/jictyvoo/multi_client_rest_api/modules/abz_1_core/internal/domain/interfaces"
+	"github.com/jictyvoo/multi_client_rest_api/modules/abz_1_core/internal/domain/mocks"
+	"github.com/jictyvoo/multi_client_rest_api/modules/abz_1_core/internal/domain/utils"
+	"strings"
+	"testing"
+)
+
+func TestContactsService_Validate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	const expectedName = "TEMP_MOCK_NAME"
+	const expectedPhone = "+55 (75) 98800-4050"
+	repoMock := mocks.NewMockContactsRepository(ctrl)
+	dtoMock := mocks.NewMockContactDTO(ctrl)
+
+	dtoMock.EXPECT().Name().Return(expectedName).AnyTimes()
+	dtoMock.EXPECT().Phone().Return(expectedPhone).AnyTimes()
+
+	// Asserts the that when
+	repoMock.
+		EXPECT().
+		GetByPhone(gomock.Any()).
+		Return(dtoMock, nil)
+
+	service := NewContactsService(repoMock)
+
+	tempDto := mocks.NewMockContactDTO(ctrl)
+	tempDto.EXPECT().Phone().Return("5575988004050").AnyTimes()
+	tempDto.EXPECT().Name().Return(strings.ToLower("NOT THE SAME NUMBER")).AnyTimes()
+
+	err := service.Add([]interfaces.ContactDTO{tempDto})
+	if err != nil && !errors.Is(err, utils.ErrContactAlreadyExists) {
+		t.Error(err)
+	}
+}
+
+func TestContactsService_Add(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	const expectedName = "TEMP_MOCK_NAME"
+	const expectedPhone = "+55 (75) 98800-4050"
+	mockList := make([]interfaces.ContactDTO, 0, 2)
+
+	repoMock := mocks.NewMockContactsRepository(ctrl)
+
+	// Asserts the that when
+	repoMock.
+		EXPECT().
+		GetByPhone(gomock.Any()).
+		DoAndReturn(func(phone string) (interfaces.ContactDTO, error) {
+			for _, dto := range mockList {
+				if dto.Phone() == phone {
+					return dto, utils.ErrContactAlreadyExists
+				}
+			}
+			return nil, nil
+		}).Times(2)
+
+	repoMock.
+		EXPECT().
+		Add(gomock.Any()).
+		Do(func(contact entities.ContactEntity) {
+			mockList = append(mockList, contact)
+		}).
+		AnyTimes()
+
+	dtoMock := mocks.NewMockContactDTO(ctrl)
+	dtoMock.EXPECT().Name().Return(expectedName).AnyTimes()
+	dtoMock.EXPECT().Phone().Return(expectedPhone).AnyTimes()
+
+	tempDto := mocks.NewMockContactDTO(ctrl)
+	tempDto.EXPECT().Phone().Return(expectedPhone).AnyTimes()
+	tempDto.EXPECT().Name().Return(strings.ToLower("NOT THE SAME NUMBER")).AnyTimes()
+
+	service := NewContactsService(repoMock)
+	err := service.Add([]interfaces.ContactDTO{tempDto, dtoMock})
+	if err != nil && !errors.Is(err, utils.ErrContactAlreadyExists) {
+		t.Error(err)
+	}
+
+	if len(mockList) != 1 {
+		t.Errorf("Expected 1 contact, got %d", len(mockList))
+	}
+}
