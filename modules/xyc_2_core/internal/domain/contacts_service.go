d81bab7f81ec9d526e128ee21db70c9ad417e83b
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-06-07 04:22:41 -0300

    :wrench: fix(xyc_2): Use `AddAll` method from repository
    - Fix transaction not doing the commit
    - Fix nil pointer access in service

--- modules/xyc_2_core/internal/domain/contacts_service.go
+++ modules/xyc_2_core/internal/domain/contacts_service.go
@@ -1,12 +1,12 @@
 package domain
 
 import (
-	"github.com/jictyvoo/multi_client_rest_api/modules/xyc_2_core/internal/core"
+	"database/sql"
+	"errors"
 	"github.com/jictyvoo/multi_client_rest_api/modules/xyc_2_core/internal/domain/entities"
 	"github.com/jictyvoo/multi_client_rest_api/modules/xyc_2_core/internal/domain/interfaces"
 	"github.com/jictyvoo/multi_client_rest_api/services/apicontracts/corerrs"
 	"github.com/jictyvoo/multi_client_rest_api/services/apicontracts/dtos"
-	"github.com/wrapped-owls/goremy-di/remy"
 )
 
 type ContactsService struct {
@@ -14,9 +14,6 @@ type ContactsService struct {
 }
 
 func NewContactsService(repository interfaces.ContactsRepository) *ContactsService {
-	if repository == nil {
-		repository = remy.Get[interfaces.ContactsRepository](core.Injector)
-	}
 	return &ContactsService{
 		repository: repository,
 	}
@@ -32,24 +29,28 @@ func (service ContactsService) Validate(dto interfaces.ContactDTO) (contact enti
 	// check if the contact already exists
 	var tempContact interfaces.ContactDTO
 	tempContact, err = service.repository.GetByPhone(contact.Phone())
-	if err == nil && (len(tempContact.Phone()) > 0) {
+	if err == nil && (tempContact != nil && len(tempContact.Phone()) > 0) {
 		err = corerrs.ErrContactAlreadyExists
 		return
+	} else if errors.Is(sql.ErrNoRows, err) {
+		err = nil
 	}
 	return
 }
 
 func (service ContactsService) Add(contacts []dtos.ContactsDTO) error {
+	validatedContacts := make([]interfaces.ContactDTO, 0, len(contacts))
 	for _, contactDTO := range contacts {
 		contact, err := service.Validate(contactDTO)
 		if err != nil {
 			return err
 		}
+		validatedContacts = append(validatedContacts, contact)
+	}
 
-		// add the contact
-		if err = service.repository.Add(contact); err != nil {
-			return err
-		}
+	// add the contact
+	if err := service.repository.AddAll(validatedContacts); err != nil {
+		return err
 	}
 	return nil
 }
